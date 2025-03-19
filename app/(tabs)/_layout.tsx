d1bfaf94c6d9773e00b5c512d5084c5ba644d9b6
Chan Min Swe <chanminswe@Chans-MacBook-Air.local>
2025-03-19 10:06:06 +0630

    before adding the calendar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,46 @@
-import { View, Text } from 'react-native'
+import { StatusBar } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function TabsLayout() {
 	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarActiveTintColor: "#007bff", // Active tab color
-				tabBarInactiveTintColor: "gray", // Inactive tab color
-				tabBarStyle: {
-					backgroundColor: "#fff", // Tab bar background
-					paddingBottom: 5, // Adjust spacing
-					height: 60, // Increase height
-				},
-			}}
-		>
-			<Tabs.Screen name='home'
-				options={{
-					title: 'Home',
-					tabBarIcon: ({ color, size }) => <Ionicons name='home' size={size} color={color} />
+		<>
+			<StatusBar barStyle="dark-content" backgroundColor="black" />
+			<Tabs
+				screenOptions={{
+					headerShown: false,
+					tabBarActiveTintColor: "#007bff",
+					tabBarInactiveTintColor: "gray",
+					tabBarStyle: {
+						backgroundColor: "#fff",
+						paddingBottom: 5,
+						height: 60,
+					},
 				}}
-			/>
-			<Tabs.Screen name='leave'
-				options={{
-					title: "Leave",
-					tabBarIcon: ({ color, size }) => <Ionicons name='people' size={size} color={color} />
-				}}
-			/>
-			<Tabs.Screen name='calander'
-				options={{
-					title: 'Calander',
-					tabBarIcon: ({ color, size }) => <Ionicons name='calendar' size={size} color={color} />
-				}}
-			/>
-		</Tabs>
+			>
+				<Tabs.Screen name='home'
+					options={{
+						title: 'Home',
+						tabBarIcon: ({ color, size }) => <Ionicons name='home' size={size} color={color} />
+					}}
+				/>
+				<Tabs.Screen name='leave'
+					options={{
+						title: "Leave",
+						tabBarIcon: ({ color, size }) => <Ionicons name='people' size={size} color={color} />
+					}}
+				/>
+				<Tabs.Screen name='calendar'
+					options={{
+						title: 'Calendar',
+						tabBarIcon: ({ color, size }) => <Ionicons name='calendar' size={size} color={color} />
+					}}
+				/>
+			</Tabs>
+		</>
+
 	)
 }
 
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
 	return (
-		<View>
-			<Text>Home</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View style={styles.homeHeader}>
+				<Text style={styles.headerText}> Hello,{"\n"} Username </Text>
+				<Image
+					style={styles.profileImage}
+					source={require('../../assets/images/react-logo.png')} />
+			</View>
+
+		</SafeAreaView>
 	)
 }
 
 export default Home
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+	},
+	homeHeader: {
+		display: 'flex',
+		flexDirection: 'row',
+		width: '100%',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingLeft: 10,
+		paddingRight: 10
+	},
+	headerText: {
+		fontSize: 22,
+		color: 'blue'
+	},
+	profileImage: {
+		width: 50,
+		height: 50
+	}
+})
